a8e8006c4e16889e44d7cd532a5d3d7dbc5eb72d
Yuu Tanaka <yutanadreamfilel.seed@gmail.com>
2020-08-26 05:36:11 +0000

    post

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-  @if (Auth::check())
-    {{ Auth::user()->name }}
-  @else
-    <div class="center jumbotron">
-      <div class="text-center">
-        <h1>Welcome to the Microposts</h1>
-        {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+@if (Auth::check())
+<div class="row">
+  <aside class="col-sm-4">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">{{ Auth::user()->name }} </h3>
+      </div>
+      <div class="card-body">
+        <img class="rounded img-fluid" src="{{ Gravatar::get(Auth::user()->email, ['size' => 500]) }}" alt="">
       </div>
     </div>
-  @endif
+  </aside>
+  <div class="col-sm-8">
+    @include('microposts.form')
+    @include('microposts.microposts')
+  </div>
+</div>
+@else
+<div class="center jumbotron">
+  <div class="text-center">
+    <h1>Welcome to the Microposts</h1>
+    {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+  </div>
+</div>
+@endif
 @endsection
